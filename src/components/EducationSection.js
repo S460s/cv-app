9c6f9cd4fc4b10fe102a34de38425d1ccd64381f
S460 <aleksandar.ta6ev@gmail.com>
2021-02-09 13:30:20 +0200

    Basic structure for Education Expirience is done

--- src/components/EducationSection.js
+++ src/components/EducationSection.js
@@ -1,10 +1,54 @@
 import React from 'react';
 
 class EducationSection extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			schoolName: '',
+			study: '',
+			from: '',
+			to: '',
+		};
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(e) {
+		const { name, value } = e.target;
+		this.setState({ [name]: value });
+	}
+
 	render() {
 		return (
 			<div>
-				<p>Education</p>
+				<h2>Educational Section</h2>
+				<input
+					type='text'
+					placeholder='School Name'
+					name='schoolName'
+					onChange={this.handleChange}
+					value={this.state.schoolName}
+				/>
+				<input
+					type='text'
+					placeholder='Title of study'
+					name='study'
+					onChange={this.handleChange}
+					value={this.state.study}
+				/>
+				<input
+					type='date'
+					name='from'
+					placeholder='From'
+					onChange={this.handleChange}
+					value={this.state.from}
+				/>
+				<input
+					type='date'
+					name='to'
+					placeholder='To'
+					onChange={this.handleChange}
+					value={this.state.to}
+				/>
 			</div>
 		);
 	}
