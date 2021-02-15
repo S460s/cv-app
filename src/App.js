256e35e4446d6563fdad4e1de365bf575895606f
S460 <aleksandar.ta6ev@gmail.com>
2021-02-15 18:12:45 +0200

    Refactored App.js with hooks an functional components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PrintProvider, { NoPrint, Print } from 'react-easy-print';
 import uniqid from 'uniqid';
 import EducationSection from './components/EducationSection';
@@ -7,87 +7,83 @@ import GeneralSection from './components/GeneralSection';
 import './styles/App.css';
 import './styles/forms.css';
 
-class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			educationIds: [],
-			experienceIds: [],
-		};
-		this.handleClick = this.handleClick.bind(this);
-		this.handleDelete = this.handleDelete.bind(this);
-	}
+const App = () => {
+	const [educationIds, setEducationIds] = useState([]);
+	const [experienceIds, setExperienceIds] = useState([]);
 
-	handleClick(type) {
-		this.setState((prevState) => {
-			return {
-				[type]: [...prevState[type], uniqid()],
-			};
-		});
-	}
+	const handleClick = (type) => {
+		if (type === 'experienceIds') {
+			setExperienceIds((prevExpIds) => [...prevExpIds, uniqid()]);
+		} else {
+			setEducationIds((prevEduIds) => [...prevEduIds, uniqid()]);
+		}
+	};
 
-	handleDelete(type, id) {
-		this.setState((prevState) => {
-			let newList = prevState[type].filter((key) => key !== id);
-			return {
-				[type]: newList,
-			};
-		});
-	}
+	const handleDelete = (type, id) => {
+		if (type === 'experienceIds') {
+			setExperienceIds((prevExpIds) => {
+				let newList = prevExpIds.filter((key) => key !== id);
+				return newList;
+			});
+		} else {
+			setEducationIds((prevEduIds) => {
+				let newList = prevEduIds.filter((key) => key !== id);
+				return newList;
+			});
+		}
+	};
 
-	render() {
-		const eduComponents = this.state.educationIds.map((id) => (
-			<EducationSection key={id} id={id} handleDelete={this.handleDelete} />
-		));
-		const expComponents = this.state.experienceIds.map((id) => (
-			<ExpirienceSection key={id} id={id} handleDelete={this.handleDelete} />
-		));
+	const eduComponents = educationIds.map((id) => (
+		<EducationSection key={id} id={id} handleDelete={handleDelete} />
+	));
+	const expComponents = experienceIds.map((id) => (
+		<ExpirienceSection key={id} id={id} handleDelete={handleDelete} />
+	));
 
-		return (
-			<div>
-				<PrintProvider>
-					<NoPrint>
-						<main>
-							<h1 className='title'>CV-App with React</h1>
+	return (
+		<div>
+			<PrintProvider>
+				<NoPrint>
+					<main>
+						<h1 className='title'>CV-App with React</h1>
+						<Print>
+							<h2 className='subTitle'>General Information</h2>
+							<GeneralSection />
+						</Print>
+						<div>
 							<Print>
-								<h2 className='subTitle'>General Information</h2>
-								<GeneralSection />
+								<h2 className='subTitle'>Educational Experience</h2>
+								{eduComponents}
 							</Print>
-							<div>
-								<Print>
-									<h2 className='subTitle'>Educational Experience</h2>
-									{eduComponents}
-								</Print>
-								<button
-									className='addBtn'
-									onClick={() => this.handleClick('educationIds')}>
-									Add
-								</button>
-							</div>
-							<div>
-								<Print>
-									<h2 className='subTitle'>Experience Section</h2>
-									{expComponents}
-								</Print>
-								<button
-									className='addBtn'
-									onClick={() => this.handleClick('experienceIds')}>
-									Add
-								</button>
-							</div>
 							<button
-								onClick={() => {
-									window.print();
-								}}
-								className='addBtn'>
-								Print
+								className='addBtn'
+								onClick={() => handleClick('educationIds')}>
+								Add
 							</button>
-						</main>
-					</NoPrint>
-				</PrintProvider>
-			</div>
-		);
-	}
-}
+						</div>
+						<div>
+							<Print>
+								<h2 className='subTitle'>Experience Section</h2>
+								{expComponents}
+							</Print>
+							<button
+								className='addBtn'
+								onClick={() => handleClick('experienceIds')}>
+								Add
+							</button>
+						</div>
+						<button
+							onClick={() => {
+								window.print();
+							}}
+							className='addBtn'>
+							Print
+						</button>
+					</main>
+				</NoPrint>
+			</PrintProvider>
+		</div>
+	);
+};
 
 export default App;
